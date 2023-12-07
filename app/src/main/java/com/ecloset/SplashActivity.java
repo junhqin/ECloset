47bf4db4ae68cbcac20d26943dfaaaa05e309bac
秦君豪 <junhqin@163.com>
2023-12-07 13:00:58 +0800

    添加了登录页面

--- app/src/main/java/com/ecloset/SplashActivity.java
+++ app/src/main/java/com/ecloset/SplashActivity.java
@@ -29,7 +29,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i =new Intent(SplashActivity.this, MainActivity.class);
+                Intent i =new Intent(SplashActivity.this, LoginActivity.class);
                 startActivity(i);
                 finish();
             }
